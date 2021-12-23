eea763edf9e2b6f066054bfa3c11a237a5d22387
Blain Crawford <blaincrawford@gmail.com>
2021-12-23 14:50:30 -0500

    finished App V1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,80 +1,66 @@
 //creating and linking html tags
-let containerDiv = document.getElementById('container');
-let body = document.querySelector('body');
-let buttons = document.querySelectorAll('.button');
-let player = document.getElementById('player');
-let zombie = document.getElementById('zombie');
-
-
-
-
+let containerDiv = document.getElementById("container");
+let body = document.querySelector("body");
+let buttons = document.querySelectorAll(".button");
+let player = document.getElementById("player");
+let zombie = document.getElementById("zombie");
 
 //creating logic for computer choices
-const computerPlay = function () {
+const zombiePlay = function () {
   let shoot = Math.floor(Math.random() * 3);
 
   if (shoot === 0) {
-    return 'rock';
+    return "rock";
   } else if (shoot === 1) {
-    return 'paper';
+    return "paper";
   } else if (shoot === 2) {
-    return 'scissors';
+    return "scissors";
   }
 };
 
 //creating logic for each round of RPS
 const playRound = function () {
   let playerShoot = this.title;
-  let computerSelection = computerPlay();
-  
+  let zombieSelection = zombiePlay();
+
   if (
-    (playerShoot === 'rock' && computerSelection === 'scissors') ||
-    (playerShoot === 'paper' && computerSelection === 'rock') ||
-    (playerShoot === 'scissors' && computerSelection === 'paper')
+    (playerShoot === "rock" && zombieSelection === "scissors") ||
+    (playerShoot === "paper" && zombieSelection === "rock") ||
+    (playerShoot === "scissors" && zombieSelection === "paper")
   ) {
-      player.textContent = parseInt(player.textContent) + 1;
+    player.textContent = parseInt(player.textContent) + 1;
   } else if (
-    (playerShoot === 'rock' && computerSelection === 'paper') ||
-    (playerShoot === 'paper' && computerSelection === 'scissors') ||
-    (playerShoot === 'scissors' && computerSelection === 'rock')
+    (playerShoot === "rock" && zombieSelection === "paper") ||
+    (playerShoot === "paper" && zombieSelection === "scissors") ||
+    (playerShoot === "scissors" && zombieSelection === "rock")
   ) {
-      zombie.textContent = parseInt(zombie.textContent) + 1;
-  }
-  
-  const handleGameEnd = function(){
-    containerDiv.innerHTML = '<h1>YOU LOSE!!!!</h1>';
-    body.classList.toggle('lose');
-    let playAgain = document.querySelector('button');
-    playAgain.addEventListener('click',()=>{
-      window.location.reload();
-    });
+    zombie.textContent = parseInt(zombie.textContent) + 1;
   }
 
-  const handleGameLoss = function(){
-    containerDiv.innerHTML = '<h1>LET THEM BUUUUUUUUURNNNNNNNNN!</h1>';
-    body.classList.toggle('win');
-    let playAgain = document.querySelector('button');
-    playAgain.addEventListener('click',()=>{
-      window.location.reload();
-    });
+  const handleGameLose = function () {
+    location.href = "/lose.html";
+  };
+
+  const handleGameWin = function () {
+    location.href = "/win.html";
+  };
+
+  const handleGameEnd = function (callBack) {
+    buttons.forEach((button) => button.removeEventListener("click", playRound));
+    setTimeout(callBack, 2000);
+  };
+
+  if (zombie.textContent === "5") {
+    handleGameEnd(handleGameLose);
   }
 
-  if(zombie.textContent === '5'){
-    buttons.forEach(button => button.removeEventListener('click', playRound));
-    setTimeout(handleGameEnd, 2000);
-  }
-
-  if(player.textContent === '5'){
-    buttons.forEach(button => button.removeEventListener('click', playRound));
-    setTimeout(handleGameLoss, 2000);
+  if (player.textContent === "5") {
+    handleGameEnd(handleGameWin);
   }
 };
 
 //adding event listeners to divs
 
 buttons.forEach((button) => {
-  button.addEventListener('click', playRound);
-  
+  button.addEventListener("click", playRound);
 });
-
-
